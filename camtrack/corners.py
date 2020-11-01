173513647a4ea2d5f75217cbc28a764f4e905a08
CHern0g0r <fyodorchernogor@gmail.com>
2020-11-01 15:00:44 +0300

    hw2

--- camtrack/corners.py
+++ camtrack/corners.py
@@ -40,10 +40,16 @@ class Tracker:
     def __init__(self, frame_0, treshold=200):
         self.frame_0 = frame_0
         self.treshold = treshold
-        self.corners, self.corner_sizes = self.find_corners(
-            frame_0,
-            quality=0.001
-        )
+        self.block_s = round(max(self.frame_0.shape) * 0.01)
+        self.win_s = self.block_s * 3
+        self.levels = 4
+        self.dist = self.block_s
+        self.quality = 0.03
+        self.max_corners = max(500, min(2000, round(
+            self.frame_0.shape[0] / self.block_s *
+            self.frame_0.shape[1] / self.block_s
+        )))
+        self.corners, self.corner_sizes = self.find_corners(frame_0)
         self.ids = np.arange(self.corners.shape[0])
         self.track_length = np.ones((self.corners.shape[0]), dtype='int')
         self.max_length = 0
@@ -54,11 +60,14 @@ class Tracker:
             frame_1,
             self.corners
         )
-        new_corners, new_sizes = self.find_corners(
-            frame_1, mask=self.get_corner_cover()
-        )
 
         m = (mask1 == 1) & (mask == 1)
+        new_m = m.reshape((-1,))
+
+        new_corners, new_sizes = self.find_corners(
+            frame_1, mask=self.get_corner_cover(tracked[m],
+                                                self.corner_sizes[new_m])
+        )
 
         self.frame_0 = frame_1
         self.corners = np.concatenate(
@@ -66,15 +75,13 @@ class Tracker:
             axis=0
         )
 
-        new_m = m.reshape((-1,))
-
         self.corner_sizes = np.concatenate(
             (self.corner_sizes[new_m], new_sizes),
             axis=0
         )
 
         last_id = self.ids[-1]
-        new_ids = np.arange(last_id, last_id + new_corners.shape[0])
+        new_ids = np.arange(last_id+1, last_id + new_corners.shape[0] + 1)
 
         self.ids = np.concatenate(
             (self.ids[new_m], new_ids),
@@ -86,88 +93,124 @@ class Tracker:
             (self.track_length[new_m], np.ones(new_corners.shape[0],)),
             axis=0
         )
+
         cur_max_lenght = self.track_length.max()
         if cur_max_lenght > self.max_length:
             self.max_length = cur_max_lenght
 
-        '''
-        Следующие строки кода позволяют фильтровать уголки по
-        минимальным собственным значениям, оставляя только нужный
-        квантиль.
-        Но из-за них обработка видео 1024x576 вместо 2 минут заняла
-        25.
-        '''
-
-        # quality_mask = self.filter_corners_mask(self.frame_0)
-        # self.corners = self.corners[quality_mask]
-        # self.corner_sizes = self.corner_sizes[quality_mask]
-        # self.ids = self.ids[quality_mask]
-        # self.track_length = self.track_length[quality_mask]
-
-    def find_layer_corners(self, frame, max_corners=10000,
-                           quality=0.01, dist=10, mask=None, blockSize=10):
+    def find_layer_corners(self, frame, mask=None):
         corners = cv2.goodFeaturesToTrack(
-            frame, max_corners, quality, dist, mask=mask, blockSize=blockSize
+            frame,
+            self.max_corners,
+            self.quality,
+            self.dist,
+            mask=mask,
+            blockSize=self.block_s
         )
         if corners is None:
             return np.empty((0, 2), dtype=float)
         corners = corners[:, 0, :]
         return corners
 
-    def find_corners(self, frame, steps=5, mask=None, quality=0.01):
+    def find_corners(self, frame, mask=None):
         layer = frame.copy()
+        if mask is not None:
+            mask_l = mask.copy()
+        else:
+            mask_l = np.full(frame.shape, 255, dtype='uint8')
         k = 1
         corners = np.empty((0, 2), dtype=float)
         sizes = np.empty((0, ), dtype=float)
 
-        for _ in range(steps):
-            layer_corners = self.find_layer_corners(layer, mask=mask,
-                                                    quality=quality)
+        for _ in range(self.levels):
+            layer_corners = self.find_layer_corners(layer, mask=mask_l)
+
+            filt_mask = np.zeros((len(layer_corners),), dtype='bool')
+            if layer_corners is not None:
+                for i, point in enumerate(layer_corners):
+                    rx, ry = np.round(point).astype(int)
+                    if mask_l[ry, rx] != 0:
+                        filt_mask[i] = True
+
+            layer_corners = layer_corners[filt_mask]
+            if np.sum(~filt_mask) > 0:
+                print('oh shit')
+
             corners = np.concatenate((corners, layer_corners * k), axis=0)
             sizes = np.concatenate(
-                (sizes, np.array([k*3] * layer_corners.shape[0])),
+                (sizes, np.array([k*self.block_s] * layer_corners.shape[0])),
                 axis=0
             )
+            mask_l = self.enlarge_corner_cover(
+                mask_l,
+                layer_corners,
+                k*self.block_s
+            )
             layer = cv2.pyrDown(layer)
-            if mask is not None:
-                mask = cv2.pyrDown(mask)
+            mask_l = cv2.pyrDown(mask_l).astype(np.uint8)
+            mask_l = np.where(mask_l < 200, 0, 255).astype('uint8')
             k *= 2
 
         return corners, sizes
 
-    def get_corner_cover(self):
+    def get_corner_cover(self, corners=None, corner_sizes=None):
+        if corners is None:
+            corners = self.corners
+            corner_sizes = self.corner_sizes
         cover = np.full(self.frame_0.shape, 255, dtype='uint8')
-        for corner, rad in zip(self.corners, self.corner_sizes):
+        for corner, rad in zip(corners, corner_sizes):
             coord = _to_int_tuple(corner)
             cover = cv2.circle(cover, coord, int(rad), color=0, thickness=-1)
         return cover
 
-    def use_optflow(self, frame_0, frame_1, points, eps=1e-2):
+    def enlarge_corner_cover(self, cover, corners, rad):
+        for corner in corners:
+            coord = _to_int_tuple(corner)
+            cover = cv2.circle(cover, coord, int(rad), color=0, thickness=-1)
+        return cover
+
+    def use_optflow(self, frame_0, frame_1, points, eps=1e-2, steps=5):
+        levels, frame_0_pyr = cv2.buildOpticalFlowPyramid(
+            (frame_0 * 255).astype(np.uint8),
+            (self.win_s, self.win_s),
+            self.levels,
+            None,
+            False
+        )
+
+        levels, frame_1_pyr = cv2.buildOpticalFlowPyramid(
+            (frame_1 * 255).astype(np.uint8),
+            (self.win_s, self.win_s),
+            self.levels,
+            None,
+            False
+        )
+
         new_points, mask, _ = cv2.calcOpticalFlowPyrLK(
-            (frame_0*255).astype(np.uint8),
-            (frame_1*255).astype(np.uint8),
+            frame_0_pyr[0],
+            frame_1_pyr[0],
             points.astype('float32').reshape((-1, 1, 2)),
             None,
-            winSize=(15, 15),
-            maxLevel=4,
+            winSize=(self.win_s, self.win_s),
+            maxLevel=self.levels,
             criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
                       10, 0.03)
         )
 
         new_points1, mask1, _ = cv2.calcOpticalFlowPyrLK(
-            (frame_1*255).astype(np.uint8),
-            (frame_0*255).astype(np.uint8),
+            frame_1_pyr[0],
+            frame_0_pyr[0],
             new_points.astype('float32').reshape((-1, 1, 2)),
             None,
-            winSize=(15, 15),
-            maxLevel=4,
+            winSize=(self.win_s, self.win_s),
+            maxLevel=self.levels,
             criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
                       10, 0.03)
         )
 
         deltas = points.reshape((-1, 1, 2)) - new_points1
         deltas = np.linalg.norm(deltas, axis=2)
-        mask = (mask == 1) & (deltas < np.quantile(deltas, 0.99, 0))
+        mask = (mask == 1) & (deltas < 0.7)
 
         return new_points, mask, mask1
 
